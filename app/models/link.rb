43a10dc05922cdab1ab041adace830b2bf9c1fdc
Filipe Abreu <filabreu@gmail.com>
2016-06-10 17:37:09 -0300

    Perform url request in Link class. Add methods to return request, request code, request message and request body.

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,28 @@ class Link
   def initialize(url)
     @url = url
   end
+
+  def url
+    @url
+  end
+
+  def request
+    @request ||= HTTParty.get(url)
+  end
+
+  def code
+    @code ||= request.code
+  end
+
+  def message
+    @message ||= request.message
+  end
+
+  def body
+    @body ||= request.body
+  end
+
+  def scrap
+    @scrap ||= git Scrapper.new(body)
+  end
 end
