01bb963594af9ed0cf6e900f093aac99c2333154
Filipe Abreu <filabreu@gmail.com>
2016-06-13 13:07:29 -0300

    Remove url from link route param, expecting the :url param after the URL path itself. Use strong params in LinksController to require the url param. Rescue from strong param ParamsRequired error to render a json error message, in ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
+
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: { error: "#{exception.param} is required" }, status: 422
+  end
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,14 @@
 class LinksController < ApplicationController
 
   def show
-    @link = Link.new(params[:url])
+    @link = Link.new(link_params)
+
+    render json: @link
   end
+
+  protected
+
+    def link_params
+      params.require(:url)
+    end
 end
