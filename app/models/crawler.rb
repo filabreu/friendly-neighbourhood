f572d6bf175ad1dd1aefe0e41a8886a407b98d56
Filipe Abreu <filabreu@gmail.com>
2016-06-13 09:54:54 -0300

    Scrap document with Nokogiri in Crawler class. Add methods to find title, description, duration and main_image with XPath. Parse duration string into minutes and seconds with regex.

--- app/models/crawler.rb
+++ app/models/crawler.rb
@@ -1,10 +1,38 @@
 class Crawler
 
   def initialize(document)
-    @document = document
+    @document = Nokogiri::HTML(document)
+    @title = title
+    @description = description
+    @main_image = main_image
+    @duration = { minutes: minutes, seconds: seconds }
   end
 
   def document
     @document
+  end
+
+  def title
+    document.xpath("/html/head/title/text()").to_s
+  end
+
+  def description
+    document.xpath("/html/head/meta[@name='description']/@content").to_s
+  end
+
+  def duration
+    document.xpath("//meta[@itemprop='duration']/@content").to_s
+  end
+
+  def minutes
+    duration.match(/\d+(?=M)/).to_s if duration.present?
+  end
+
+  def seconds
+    duration.match(/\d+(?=S)/).to_s if duration.present?
+  end
+
+  def main_image
+    document.xpath("//meta[@property='og:image']/@content").to_s
   end
 end
